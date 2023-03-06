edd16aa2767ee00662b01aa8c9d1827bc4fff736
moeri0428 <moeri0428@outlook.jp>
2023-03-06 22:32:14 +0900

    create app

---
+++ README.md
@@ -0,0 +1,68 @@
+# ジュリア集合の生成と絵画
+
+実数部最小値min_x、実数部最大値max_x、虚数部最小値min_y、虚数部最大値max_y、複素定数comp_constを入力することで、ジュリア集合を生成し絵画することができます。
+
+
+# Features
+
+* 採用した臨界値：100
+    * 漸化式を100回計算するうちに、無限大（1垓以上）になれば発散、そうでなければ収束と定義
+
+* 色付け：pythonのライブラリであるmatplotlibのcolormap "seismic"を使用
+    * n=0が濃青、50が白、100が濃赤であり、その間がグラデーションとなっております。
+
+https://matplotlib.org/stable/tutorials/colors/colormaps.html#diverging
+
+* 分割数：1000
+    * 実数部、虚数部ともに、与えられた最大値と最小値の間を1000分割して計算を行います。
+
+
+## エラーについて
+* error1：数字以外の文字列が入力された場合（未入力含む）
+* error2：最大値と最小値の大小関係がおかしい場合
+
+どちらも400エラーを返し、エラーの内容をアラートで表示します。
+
+
+# Requirement
+
+* python 3.10.10
+
+*numbaがpython3.11以前にしか対応していないため*
+
+ライブラリ
+* Flask 2.2.3
+* matplotlib 3.7.0
+* numba 0.56.4
+* numpy 1.23.5
+
+
+# Installation
+
+```bash
+pip install flask
+pip install matplotlib
+pip install numpy
+pip install numba
+```
+
+# Note
+
+* Macにて作成したため、Windowsでの動作は確認できておりません。
+* ジュリア集合の図形作成の実装に関して、以下のサイトを参考にさせていただきました。
+https://watlab-blog.com/2020/06/28/julia-set/
+
+### 警告の非表示について
+```app.py
+import warnings
+warnings.simplefilter('ignore')
+```
+* numbaをnumpyの計算を高速化するために使用していますが、numpyの機能の一部はnumbaに対応しておらず、API実行時ターミナルに警告表示が出ますが、動作には問題がありません。
+* 全てをnumbaに対応している形式で書いた方が速いのですが、コードが長くなってしまうこと、現在のコードでも十分な速さであることから、現在のコードを採用しました。
+* 以上のことから、警告を非表示にしております。
+
+
+# Author
+
+* 荒井 萌里
+* moeri0428.oo@icloud.com
---
+++ app.py
@@ -0,0 +1,113 @@
+import base64
+import numpy as np
+from numba import jit
+from io import BytesIO
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+
+from flask import Flask, render_template, request
+
+# 一部numbaのjitに対応していない部分があり、警告が表示されるため非表示
+import warnings
+warnings.simplefilter('ignore')
+
+
+# ジュリア集合の計算
+@jit
+def julia(z_real, z_imag, n_max, a, b):
+    # 実部Reと虚部Imの組み合わせを計算
+    Re, Im = np.meshgrid(z_real, z_imag)
+    # 組み合わせの総数
+    n_grid = len(Re.ravel())
+    # データ格納用空配列
+    z = np.zeros(n_grid)
+
+    # ジュリア集合に属するか否かを計算し、Zに格納
+    for i in range(n_grid):
+        c = complex(a, b)
+        n = 0
+        z0 = complex(Re.ravel()[i], Im.ravel()[i])
+
+        # z0が無限大(1垓以上)になるか、最大繰り返し数になるまでループする
+        while np.abs(z0) < 1e20 and not n == n_max:
+            # 漸化式を計算
+            z0 = z0 ** 2 + c
+            # 繰り返し数を増分
+            n += 1
+
+        # z0が無限大に発散する場合はn, 収束する場合は0を格納
+        if n == n_max:
+            z[i] = 0
+        else:
+            z[i] = n
+
+    # 2次元配列にリシェイプ
+    z = np.reshape(z, Re.shape)
+    # imshow()で上下逆になるので予め上下反転
+    z = z[::-1]
+    return z
+
+
+app = Flask(__name__)
+
+@app.route('/julia')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/julia/post', methods=["POST"])
+def post():
+    try:
+        min_x = float(request.form["min_x"])
+        max_x = float(request.form["max_x"])
+        min_y = float(request.form["min_y"])
+        max_y = float(request.form["max_y"])
+        a = float(request.form["comp_const_x"])
+        b = float(request.form["comp_const_y"])
+    # 数値が入力されていないエラー
+    except ValueError:
+        return 'error1', 400
+    
+    # 最大値が最小値よりも小さいエラー
+    if min_x >= max_x or min_y >= max_y:
+        return 'error2', 400
+
+    # 分割数
+    resolution = 1000
+
+    # 実部と虚部の軸データ配列
+    z_real = np.linspace(min_x, max_x, resolution)
+    z_imag = np.linspace(min_y, max_y, resolution)
+    # 最大繰り返し数
+    n_max = 100
+
+    # 計算
+    z = julia(z_real, z_imag, n_max, a, b)
+
+    # 図形作成
+    plt.figure()
+    plt.imshow(z, cmap='seismic',
+               norm=Normalize(vmin=0, vmax=n_max),
+               extent=[min_x, max_x, min_y, max_y])
+    # 軸を消去
+    plt.axis('off')
+    plt.tight_layout()
+
+    # png画像を保存
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png", bbox_inches='tight', pad_inches=0)
+    buffer.seek(0)
+    img = buffer.getvalue()
+
+    # png画像をbase64に変換
+    graph = base64.b64encode(img)
+    buffer.close()
+
+    return graph
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
